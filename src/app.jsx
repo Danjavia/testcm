913b53b4ef5655275182703f19363faa0074afe4
Danjavia <danjavia.iconshock@gmail.com>
2016-01-13 22:18:17 -0500

    ready to fun

--- src/app.jsx
+++ src/app.jsx
@@ -1,54 +1,20 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var ReactRouter = require('react-router');
-var HashHistory = require('react-router/lib/hashhistory');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
+import React from 'react';  
+import ReactDOM from 'react-dom';
 
-// Components
-var Home = require( './home' );
-var Login = require( './login' );
-var Favorites = require( './favorites' );
+class App extends React.Component {  
+  constructor (props) {
+      super(props);
+      this.state = {};
+  }
 
-// Firebase area
-global.ReactFire = require('reactfire');
-global.Firebase = require('firebase');
-global.RootUrl = 'https://therion.firebaseIO.com/';
+  render() {
+    return (
+        <div>
+            Hello Danny!
+        </div>
+    );
+  }
 
-// Analytics
-// global.Woopra = require( 'woopra' );
-// global.woopra = new Woopra( 'imaginarium.danjavia.xyz' );
+}
 
-var ua = require( 'universal-analytics' );
-
-// global variables
-global[ 'globalState' ] = {};
-
-// include analytics code here
-var visitor = ua( 'UA-71033807-1' );
-
-visitor.pageview("/").send();
-
-
-// APP
-var App = React.createClass({
-
-  	render: function() {
-		return (<div>
-	  		{this.props.children ? this.props.children: <Home/>}
-		</div>)
-  	}
-});
-
-var routes = (
-	<Router history={new HashHistory}>
-	    <Route path="/" component={App}>
-	      	<Route path="login" component={Login}/>
-	      	<Route path="favorites" component={Favorites}>
-	      	</Route>
-	      	<Route path="*" component={Home}/>
-	    </Route>
-	</Router>
-);
-
-ReactDOM.render(routes, document.getElementById('content'));
+ReactDOM.render(<App />, document.querySelector("#content")); 
