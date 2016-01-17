900cb920618b09b90fab1b8273316aca3537c4dd
Danjavia <danjavia.iconshock@gmail.com>
2016-01-17 00:58:31 -0500

    events

--- src/componentsv2/product/product.js
+++ src/componentsv2/product/product.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router';
 class Product extends Component {
 	render() {
 		return (
-			<div className="col s12 m6 l4 product hoverable" id={this.props.data.id}>
+			<div className="col s12 m6 l4 product" id={this.props.data.id}>
 				<Link className="teal-text darken-4" to={`/product/${this.props.data.id}`}><h3 className="product__name">{this.props.data.name}</h3></Link>
-				<Link to={`/product/${this.props.data.id}`}><img src={this.props.data.image} className="product__image" alt="placeholder+image" /></Link>
+				<Link to={`/product/${this.props.data.id}`}><img src={this.props.data.image} className="product__image hoverable" alt="placeholder+image" /></Link>
 				<p className="product__description">
 					{this.props.data.description}
 				</p>
 				<strong className="teal-text darken-4">Price: $<a className="product__price right-align teal-text darken-4">{this.props.data.price}</a> USD</strong>
-				<Link to="login" className="btn waves-effect waves-light teal darken-4 right product__buy">Buy now</Link>
+				<Link to={`/product/${this.props.data.id}`} className="btn waves-effect waves-light teal darken-4 right product__buy">Buy now</Link>
 			</div>
 		)
 	}
