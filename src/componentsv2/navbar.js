0b819129a29981049bd1e1c178bd732149ad891c
Danjavia <danjavia.iconshock@gmail.com>
2016-01-17 18:44:19 -0500

    fix login

--- src/componentsv2/navbar.js
+++ src/componentsv2/navbar.js
@@ -24,13 +24,24 @@ class Navbar extends React.Component {
 		window.location.href = '/'
 	}
 
+	cleanOrders ( e ) {
+		
+		e.preventDefault()
+
+		delete localStorage.saleProduct
+		delete localStorage.promo
+		delete localStorage.pid
+
+		window.location.href = '/'
+	}
+
 	render () {
 		return (
 			<nav className="grey darken-4">
 	            <div className="nav-wrapper container">
-	                <Link to="/" className="brand-logo">Woopra</Link>
+	                <Link to="/" className="brand-logo" onClick={ this.cleanOrders.bind( this ) }>Woopra</Link>
 	                <ul className="right hide-on-med-and-down app-menu">
-	                    <li><Link to='/'>Home</Link></li>
+	                    <li><Link to='/' onClick={ this.cleanOrders.bind( this ) }>Home</Link></li>
 	                    { this.state.auth ? <li><Link to='/profile'>Profile</Link></li> : null }
 	                    { this.state.auth ? <li><a href="/" onClick={this.logout.bind( this )}>Logout</a></li> : <li><Link to='/login'>Login</Link></li> }
 	                </ul>
