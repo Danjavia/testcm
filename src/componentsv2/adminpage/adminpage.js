2bcb06d04c12f6d73c1ba3d1196d8e3fab470190
Danny Viasus(Danjavia) <danny.viasus@comparamejor.com>
2016-01-18 13:23:34 -0500

    trackjs

--- src/componentsv2/adminpage/adminpage.js
+++ src/componentsv2/adminpage/adminpage.js
@@ -4,10 +4,57 @@ import React, { PropTypes, Component } from 'react'
 
 class AdminPage extends Component {
 
+	componentDidMount () {
+
+		let obj = {
+		    website: "ecommerce-site.com",
+		    date_format: "dd-MM-yyyy",
+		    start_day: 29,
+		    end_day: 0,
+		    limit: 15,
+		    goals: [{
+		        operator: "AND",
+		        name: "Confirmed",
+		        filters: [{
+		            scope: "actions",
+		            value: "pay",
+		            match: "contains",
+		            key: "name"
+		        }]
+		    }],
+		 
+		    "order_by": 0,
+		    "segments": [],
+		    "group_by": {
+		        "scope": "visits",
+		        "key": "day"
+		    }
+		};
+		 
+		$.ajax({
+		    type: 'GET',
+		    url: 'http://www.woopra.com/rest/2.4/funnels',
+		    crossDomain: true,
+
+		    beforeSend: function (xhr) {
+		        xhr.setRequestHeader('Authorization', 'Basic '+encodeBase64(appID+':'+secretKey));
+		    },
+
+		    data: {
+		        request: JSON.stringify(obj)
+		    },
+
+		    success: function (text) {
+
+		        alert(JSON.stringify(text));
+		    }
+		});
+	}
+
 	render() {
 		return (
 			<div>
-				Welcome to the admin page
+				Welcome to the admin page test
             </div>
         )
 	}
