f82cecf8a13904230e2606651e3d21d4443a575c
Danny Viasus(Danjavia) <danny.viasus@comparamejor.com>
2016-01-18 08:52:11 -0500

    ¨¨link

--- src/componentsv2/banner/sidebanner.js
+++ src/componentsv2/banner/sidebanner.js
@@ -10,7 +10,7 @@ class Sidebanner extends Component {
 			<div className="banner white-text">
 				<div className="ads">
 					<h5>Advertising</h5>
-					{ ! localStorage.signed ? <Ad image="assets/images/android.jpg"/> : null }
+					{ ! localStorage.signed ? <Ad image="assets/images/Android.jpg"/> : null }
 					<Ad image="assets/images/swift.jpg" link="http://localhost:3000/#/product/569bea3cb448a9d012719342" text="Get this Course from $10 USD"/>
 				</div>
 	      	</div>
